06a46141f7a5b19589151795ad1c9f8281ba4a4a
unknown <khannsaab01@gmail.com>
2025-02-12 14:57:35 +0500

    in app.js creating connection between html and js by creating variables to get html elements like form, input field and UL and in index.html commenting out list item so that we can update the list through JS by user input. create an array for all todos, add addeventlistener on form on submit action. create function to add todos in todo Array, creat function to creat list item and to add it in UL use append function to display on page.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert("test")
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoListUL = document.getElementById("todo-list");
+
+let allTodos = [];
+
+todoForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo() {
+    const todoText = todoInput.value.trim();
+    if (todoText.length > 0) {
+        allTodos.push(todoText);
+        createTodoItem(todoText);
+        todoInput.value = "";
+        // console.log(allTodos);
+    }
+}
+
+function createTodoItem(todo) {
+    const todoLI = document.createElement("li");
+    todoLI.innerText = todo;
+    todoListUL.append(todoLI);
+}
